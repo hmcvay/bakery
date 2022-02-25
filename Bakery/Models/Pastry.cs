2f57dc2ab85f5cb7ee5483973a894fec0f730ec1
hmcvay <hayleymcvay8@gmail.com>
2022-02-25 15:28:43 -0800

    Add Pastry list to class, and instances to constructor.

--- Bakery/Models/Pastry.cs
+++ Bakery/Models/Pastry.cs
@@ -1,13 +1,16 @@
 using System;
+using System.Collections.Generic;
 
 namespace Bakery.Models
 {
   public class Pastry
   {
     public int PastryCount { get; set; }
+    private static List<Pastry> _instances = new List<Pastry>{};
     public Pastry(int pastryCount)
     {
       PastryCount = pastryCount;
+      _instaces.Add(this);
     }
 
     public static int GetPastryPrice(int pastryCount)
