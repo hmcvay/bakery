57848984a22f65466bcc5b5bfe6ab55b691328ae
hmcvay <hayleymcvay8@gmail.com>
2022-02-25 08:30:24 -0800

    Add boiletplate

---
+++ Bakery.Tests/ModelTests/Bakery.Tests.cs
@@ -0,0 +1,18 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bakery.Models;
+
+namespace Bakery.TestTools
+{
+  [TestClass]
+
+  public class BakeryTests
+  {
+    [TestMethod]
+
+    public void WhatTesting_WhatBehavior_WhatType()
+    {
+      
+    }
+  }
+}
