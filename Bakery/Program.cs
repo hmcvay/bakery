fa8269a77165f0fbd2da1e30b35f6bbca0a25052
hmcvay <hayleymcvay8@gmail.com>
2022-02-25 14:13:57 -0800

    LUNCH: 1 hour lunch, and continue program buildout and else/if branching.

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Bakery.Models;
 
 namespace Bakery
@@ -10,14 +11,44 @@ namespace Bakery
     {
       Console.WriteLine("Welcome to Pierre's Bakery!");
       Console.WriteLine("-⎽__⎽-⎻⎺⎺⎻-⎽__⎽--⎻⎺⎺⎻--⎽__⎽-⎻⎺⎺⎻-⎽");
-      Console.WriteLine("Would you like to see our prices?");
+      Console.WriteLine("BREAD: $5 each / buy 2 get 1 FREE");
+      Console.WriteLine("PASTRY: $2 each, or 3 for $5");
+      Console.WriteLine("Would you like to buy today?");
       Console.WriteLine("Type 'Y' for YES and 'N' for NO");
       string response = Console.ReadLine().ToLower();
       if (response == "y")
       {
         Console.WriteLine("FANTASTIC!");
         Console.WriteLine("-⎽__⎽-⎻⎺⎺⎻-⎽__⎽--⎻⎺⎺⎻--⎽__⎽-⎻⎺⎺⎻-⎽");
-        Console.WriteLine("Here are our prices for BREAD and PASTRY:");
+        Console.WriteLine("Please choose PASTRY or BREAD");
+        Start();
+        static void Start()
+        {
+          string choice = Console.ReadLine().ToLower();
+          if (choice == "bread")
+          {
+            Console.WriteLine("BREAD: $5 each / buy 2 get 1 FREE");
+            Console.WriteLine("Please enter how many loaves to order:");
+            int userInput = Int32.Parse(Console.ReadLine());
+            int breadAmountTotal = Bread.GetUnitPrice(userInput);
+            Console.WriteLine(userInput + " loaves of bread comes out to $" + breadAmountTotal);
+            Console.WriteLine("Would you like add more BREAD or PASTRY to your oder?");
+            Start();
+          }
+          else if (choice == "pastry")
+          {
+
+          }
+          else if (choice == "done")
+          {
+            
+          }
+          else
+          {
+            Console.WriteLine("please enter valid response. Either BREAD or PASTRY or DONE");
+            Start();
+          }
+        }
       }
       else
       {
