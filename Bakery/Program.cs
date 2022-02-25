985127664a0db44f54ee60548a06891dea1cbf8f
hmcvay <hayleymcvay8@gmail.com>
2022-02-25 13:01:43 -0800

    Add welcome message to main() and begin program build out.

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -8,7 +8,21 @@ namespace Bakery
   {
     public static void Main()
     {
-      
+      Console.WriteLine("Welcome to Pierre's Bakery!");
+      Console.WriteLine("-⎽__⎽-⎻⎺⎺⎻-⎽__⎽--⎻⎺⎺⎻--⎽__⎽-⎻⎺⎺⎻-⎽");
+      Console.WriteLine("Would you like to see our prices?");
+      Console.WriteLine("Type 'Y' for YES and 'N' for NO");
+      string response = Console.ReadLine().ToLower();
+      if (response == "y")
+      {
+        Console.WriteLine("FANTASTIC!");
+        Console.WriteLine("-⎽__⎽-⎻⎺⎺⎻-⎽__⎽--⎻⎺⎺⎻--⎽__⎽-⎻⎺⎺⎻-⎽");
+        Console.WriteLine("Here are our prices for BREAD and PASTRY:");
+      }
+      else
+      {
+        Console.WriteLine("you make me sad (｡•́︿•̀｡)");
+      }
     }
   }
 }
