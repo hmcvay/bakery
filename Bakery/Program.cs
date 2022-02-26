2e18f0f3b952a6a6d73d659254e28e9019fbccbd
hmcvay <hayleymcvay8@gmail.com>
2022-02-25 16:33:54 -0800

    WIP: styling console app

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -9,50 +9,70 @@ namespace Bakery
   {
     public static void Main()
     {
+      Console.WriteLine("----------------------------------");
       Console.WriteLine("Welcome to Pierre's Bakery!");
-      Console.WriteLine("-⎽__⎽-⎻⎺⎺⎻-⎽__⎽--⎻⎺⎺⎻--⎽__⎽-⎻⎺⎺⎻-⎽");
+      Console.WriteLine("----------------------------------");
       Console.WriteLine("BREAD: $5 each / buy 2 get 1 FREE");
       Console.WriteLine("PASTRY: $2 each, or 3 for $5");
+      Console.WriteLine("----------------------------------");
       Console.WriteLine("Would you like to buy today?");
       Console.WriteLine("Type 'Y' for YES and 'N' for NO");
+      Console.WriteLine("----------------------------------");
       string response = Console.ReadLine().ToLower();
       if (response == "y")
       {
+        Console.WriteLine("----------------------------------");
         Console.WriteLine("FANTASTIC!");
-        Console.WriteLine("-⎽__⎽-⎻⎺⎺⎻-⎽__⎽--⎻⎺⎺⎻--⎽__⎽-⎻⎺⎺⎻-⎽");
+        Console.WriteLine("----------");
         Console.WriteLine("Please choose PASTRY or BREAD");
+        Console.WriteLine("----------------------------------");
         Start();
         static void Start()
         {
           string choice = Console.ReadLine().ToLower();
           if (choice == "bread")
           {
+            Console.WriteLine("------");
             Console.WriteLine("BREAD: $5 each / buy 2 get 1 FREE");
+            Console.WriteLine("----------------------------------");
             Console.WriteLine("Please enter how many loaves to order:");
-            int userInput = Int32.Parse(Console.ReadLine());
-            int breadAmountTotal = Bread.GetUnitPrice(userInput);
-            Console.WriteLine(userInput + " loaves of bread comes out to $" + breadAmountTotal);
-            Console.WriteLine("Would you like add more BREAD or PASTRY to your oder?");
+            int breadAmount = Int32.Parse(Console.ReadLine());
+            int breadAmountTotal = Bread.GetUnitPrice(breadAmount);
+            Console.WriteLine(breadAmount + " loaves of bread = $" + breadAmountTotal);
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("Would you like add more BREAD or PASTRY to your oder? or are you DONE?");
+            Console.WriteLine("----------------------------------");
             Start();
           }
           else if (choice == "pastry")
           {
-
+            Console.WriteLine("PASTRY: $2 each, or 3 for $5");
+            Console.WriteLine("Please enter how many pastries to order:");
+            int pastryAmount = Int32.Parse(Console.ReadLine());
+            int pastryAmountTotal = Pastry.GetPastryPrice(pastryAmount);
+            Console.WriteLine(pastryAmount + " pastries = $" + pastryAmountTotal);
+            Console.WriteLine("Would you like add more BREAD or PASTRY to your oder? or are you DONE?");
+            Start();
           }
           else if (choice == "done")
           {
-            
+            // int pastryAmountTotal = Pastry.GetPastryPrice(pastryAmount);
+            // int breadAmountTotal = Bread.GetUnitPrice(breadAmount);
+            // int grandTotal = pastryAmountTotal + breadAmountTotal;
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("Your grand total is: $" );
           }
           else
           {
-            Console.WriteLine("please enter valid response. Either BREAD or PASTRY or DONE");
+            Console.WriteLine("please enter valid response. Either: BREAD, PASTRY, or DONE");
             Start();
           }
         }
       }
       else
       {
-        Console.WriteLine("you make me sad (｡•́︿•̀｡)");
+        Console.WriteLine("You make me sad (｡•́︿•̀｡)");
+        Console.WriteLine("See you another time.");
       }
     }
   }
